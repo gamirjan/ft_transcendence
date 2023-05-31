f5624c44f1de992a6f2cd22a0ebed5239c642fae
armanarut <armanaruta@gmail.com>
2023-05-31 21:23:04 +0400

    chat

--- backend/src/chat.server.ts
+++ backend/src/chat.server.ts
@@ -9,8 +9,51 @@ export class ChatServer {
         console.log("socket");
       return data;
     }
-
-
-
-
 }
+
+var express = require('express');
+var app = express();
+app.use(express.static('public')); 
+var http = require('http').Server(app);
+var port = process.env.PORT || 3001;
+
+// setup my socket server
+var io = require('socket.io')(http);
+ 
+io.on('connection', function(socket) {
+
+    socket.on("room",function(room) {
+        console.log("Room: " + room);
+        socket.join(room);
+    });
+    
+    console.log('New connection');
+    const count = io.engine.clientsCount;
+    console.log("Connected clients: " + count);
+    io.emit('participants',count);
+    // to do: count clients in a room 
+    // https://stackoverflow.com/questions/31468473/how-to-get-socket-io-number-of-clients-in-room
+
+    // Called when the client calls socket.emit('message')
+    socket.on('message', function(obj) {
+        console.log('Chatlog - Room: ' + obj.room + ' | ' + obj.userid + ': ' + obj.msg);     
+        // socket.broadcast.emit('message', msg); // to all, but the sender
+        io.to(obj.room).emit('message',obj); // to all, including the sender
+    });
+
+    // Called when a client disconnects
+    socket.on('disconnect', function() {
+        console.log('Disconnection');
+        const count = io.engine.clientsCount;
+        console.log("Connected clients: " + count);
+        io.emit('participants',count);
+    });
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/default.html');
+});
+
+http.listen(port, function() {
+    console.log('listening on *: ' + port);
+});
--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,8 +1,63 @@
 import React from "react"
 import { Layout } from "./Layout";
 import profile from '@SRC_DIR/assets/images/profile.svg';
+import { io } from 'socket.io-client'
+
+// setup my socket client
+var socket = io();
 
 
+// read room in querystring
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const room = urlParams.get('room');
+
+const generateRandomString = (length=6) => Math.random().toString(20).substr(2, length);
+
+const generateRandomDarkColor = () => {
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+      color += Math.floor(Math.random() * 10);
+  }
+  return color;
+}
+
+window.onload = function () {
+  socket.emit('room', room);
+  let userid  : string | null = "";
+  if (localStorage.getItem("userid")) {
+    userid = localStorage.getItem("userid");
+  } else {
+    userid = generateRandomString(8);
+    localStorage.setItem("userid",userid);
+    localStorage.setItem("userColor",generateRandomDarkColor());
+  } 
+
+ $("form").on('submit', function(event){
+    event.preventDefault();
+    let msg = $("#msg").val();
+    msg =  msg.replace(/(<([^>]+)>)/gi, "");
+    const userid = localStorage.getItem("userid");
+    const userColor = localStorage.getItem("userColor");
+    console.log("sending msg: " + msg + " from " + userid);
+    socket.emit('message', {"msg" : msg, "userid" : userid, "room": room, "userColor": userColor});
+    $("#msg").val("");
+  });
+  
+ socket.on('message', function (obj) {
+    // let prevChat = $("#chatBoard").html();
+    // const ts = new Date().toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute:'2-digit'});
+    // $("#chatBoard").html(prevChat + '<div class="msgRow"><span class="userid" style="color:'+obj.userColor+'">' + obj.userid + "</span><br />" + obj.msg + '<span class="ts">' + ts +'</span></div>');
+    // var chatboard = document.getElementById("chatBoard");
+    // chatboard.scrollTop = chatboard.scrollHeight;
+    // console.log(prevChat + obj.userid + ": " + obj.msg);
+    console.log(obj.userid + ": " + obj.msg);
+  });
+
+  socket.on('participants', function(count) {
+    $("#participants").html(count);
+  });
+}
 
 const Chat = () => {
     return ( 
@@ -261,13 +316,16 @@ const Chat = () => {
                                 <button className="w-1/12 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-5 px-4 rounded-xl">
                                     Game
                                 </button>
-
-                                <input
-                                className="w-10/12  bg-gray-300 py-5 px-3 rounded-xl"
-                                type="text"
-                                placeholder="type your message here..."
-                                />
-                                <button className="w-1/12 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-5 px-4 rounded-xl">
+                                    <input 
+                                    className="w-10/12  bg-gray-300 py-5 px-3 rounded-xl"
+                                    type="text" 
+                                    placeholder="your message..." 
+                                    aria-label="your message..." 
+                                    aria-describedby="basic-addon2" 
+                                    id="msg" required
+                                    />
+                                <button 
+                                    id="sendMsg" className="w-1/12 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-5 px-4 rounded-xl">
                                     Send
                                 </button>
                             </div>
