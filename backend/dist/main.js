f996fede00a22d64ede2d0ae24a2960ba4fbe94d
ababoomian <baboomian53@gmail.com>
2023-07-04 17:34:34 +0400

    backend build error resolved

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -2,12 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const platform_socket_io_1 = require("@nestjs/platform-socket.io");
 const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
         origin: "*"
     });
+    app.useWebSocketAdapter(new platform_socket_io_1.IoAdapter(app));
     const config = new swagger_1.DocumentBuilder()
         .setTitle('Transcendence')
         .setDescription('Transcendence API')
