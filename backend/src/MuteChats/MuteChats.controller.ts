2d63ee37438feabc40d5cacd91185e1c339a8681
Arm_G <armen_g@purlin.com>
2023-07-15 20:50:43 +0400

    add block check

--- backend/src/MuteChats/MuteChats.controller.ts
+++ backend/src/MuteChats/MuteChats.controller.ts
@@ -7,6 +7,11 @@ import { MuteChatsService } from './MuteChats.service';
 export class MuteChatsController {
   constructor(private readonly muteChatsService: MuteChatsService) {}
 
+  @Get('/:userid/:checkuserid')
+  async CheckMute(@Param('userid') userid: number, @Param('checkuserid') checkuserid: number, @Res() res): Promise<object> {
+    return res.send(await this.muteChatsService.CheckMuteStatus(userid, checkuserid));
+  }
+
   @Post('mute')
   async MuteUser(@Body() payload: { callinguserid: number, userid: number }, @Res() res): Promise<Mutechat> {
     const { callinguserid, userid } = payload;
